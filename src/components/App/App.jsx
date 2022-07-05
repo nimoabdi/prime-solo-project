ebc27887f1e6cb64eab38f65b99922b0e2c9a1ff
Nimo Abdi <nimoabdi318@gmail.com>
2022-07-05 18:37:58 -0500

    pathing

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -49,6 +49,12 @@ function App() {
             <AboutPage />
           </Route>
 
+          <Route exact
+            path="/series"
+          >
+          <SeriesView />  
+            
+          </Route>
           {/* For protected routes, the view could show one of several things on the same route.
             Visiting localhost:3000/user will show the UserPage if the user is logged in.
             If the user is not logged in, the ProtectedRoute will show the LoginPage (component).
--- src/components/SeriesView/SeriesView.jsx
+++ src/components/SeriesView/SeriesView.jsx
@@ -1,8 +1,21 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
 function SeriesView() {
+    const dispatch = useDispatch;
+    const series = useSelector(store => store.series)
+    console.log(series);
+    useEffect(() => {
+        dispatch({
+            type:'FETCH_SERIES'
+
+        })
+    },[])
+
     return (
-        <>
-        
-        </>
+        <div>
+            <h1>Series here👽</h1>
+        </div>
     )
 }
 export default SeriesView;
