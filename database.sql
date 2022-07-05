7be8566708a79d189027ed88802b41574e4d6d17
Nimo Abdi <nimoabdi318@gmail.com>
2022-07-05 16:17:01 -0500

    routes for api set up

--- database.sql
+++ database.sql
@@ -8,3 +8,22 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "series" (
+	"id" SERIAL PRIMARY KEY NOT NULL
+);
+	
+CREATE TABLE "user_series"(
+	"id" SERIAL PRIMARY KEY,
+	"user_id" INT REFERENCES "user",
+	"series_id" INT REFERENCES "series",
+	"status" VARCHAR (200),
+	"rating" INTEGER
+);	
+
+CREATE TABLE "user_friends" (
+	"id" SERIAL PRIMARY KEY,
+	"user_following" INT,
+	"user_followers" INT
+);		
+	
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -19,6 +19,7 @@ import InfoPage from '../InfoPage/InfoPage';
 import LandingPage from '../LandingPage/LandingPage';
 import LoginPage from '../LoginPage/LoginPage';
 import RegisterPage from '../RegisterPage/RegisterPage';
+import SeriesView from '../SeriesView/SeriesView';
 
 import './App.css';
 
